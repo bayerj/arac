0368ff40549509e209990f805046dbf34c10667f
Justin S Bayer <bayer.justin@googlemail.com>
2009-01-10 17:17:35 +0100

    - added some more tests for the siwggenerated cppbridge

--- src/python/arac/tests/test_cppbridge.py
+++ src/python/arac/tests/test_cppbridge.py
@@ -22,7 +22,31 @@ class TestStructure(TestCase):
         l1 = arac.cppbridge.LinearLayer(2)
         l2 = arac.cppbridge.LinearLayer(2)
         c = arac.cppbridge.FullConnection(l1, l2, params, derivs, 0, 2, 0, 2)
+
+        self.assertEqual(c.get_incomingstart(), 0)
+        self.assertEqual(c.get_incomingstop(), 2)
+        self.assertEqual(c.get_outgoingstart(), 0)
+        self.assertEqual(c.get_outgoingstop(), 2)
+
         self.assert_(c.this is not None)
+        
+    def testNetwork(self):
+        l1 = arac.cppbridge.LinearLayer(2)
+        l2 = arac.cppbridge.LinearLayer(2)
+        
+        params = scipy.array((1, 2, 3, 4), dtype='float64')
+        derivs = scipy.zeros(4)
+        con = arac.cppbridge.FullConnection(l1, l2, params, derivs, 0, 2, 0, 2)
+        # con = arac.cppbridge.IdentityConnection(l1, l2, 0, 2, 0, 2)
+        
+        net = arac.cppbridge.Network()
+        net.add_module(l1, arac.cppbridge.Network.InputModule)
+        net.add_module(l2, arac.cppbridge.Network.OutputModule)
+        net.add_connection(con)
+        inpt = scipy.array((3., 4.))
+        result = scipy.zeros(2)
+        net.activate(inpt, result)
+        self.assertArrayNear(result, scipy.array((9, 28)))
 
 
 if __name__ == "__main__":
