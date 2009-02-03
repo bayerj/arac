1bb681b5743ad70b7fad01cc56b15920c9042094
Justin S Bayer <bayer.justin@googlemail.com>
2009-02-03 14:08:11 +0100

    Moved to creating datasets out of templates.

--- src/cpp/tests/test_datasets.cpp
+++ src/cpp/tests/test_datasets.cpp
@@ -118,7 +118,7 @@ TEST(TestDataset_array_array, TestConstruction)
     data_p[10] = 1;
     data_p[11] = 0;
     
-    SupervisedDataset<double*, double*> ds(2, 1);
+    SupervisedSimpleDataset ds(2, 1);
     ds.append(data_p, data_p + 2);
     ds.append(data_p + 3, data_p + 5);
     ds.append(data_p + 6, data_p + 8);
