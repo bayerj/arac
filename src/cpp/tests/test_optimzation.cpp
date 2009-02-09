fea2a767c279436370b117dd3a8acdb30601e2ae
Justin S Bayer <bayer.justin@googlemail.com>
2009-02-09 18:49:43 +0100

    Added tests for backpropagation of sequences with double target and sequences with sequence targets.

--- src/cpp/tests/test_optimzation.cpp
+++ src/cpp/tests/test_optimzation.cpp
@@ -13,7 +13,7 @@ using namespace arac::datasets;
 using namespace arac::optimization;
 
 
-TEST(TestBackprop, TestStochasticStep)
+TEST(TestBackprop, TestStochasticStepDoubleDouble)
 {
     LinearLayer* inlayer_p = new LinearLayer(1);
     LinearLayer* outlayer_p = new LinearLayer(2);
@@ -50,5 +50,88 @@ TEST(TestBackprop, TestStochasticStep)
     EXPECT_DOUBLE_EQ(3, con_p->get_parameters()[1])
         << "Parameters not learned correctly.";
 }
+
+
+TEST(TestBackprop, TestStochasticStepSequenceDouble)
+{
+    LinearLayer* inlayer_p = new LinearLayer(1);
+    LinearLayer* outlayer_p = new LinearLayer(1);
+    inlayer_p->set_mode(Component::Sequential);
+    outlayer_p->set_mode(Component::Sequential);
+    
+    FullConnection* con_p = new FullConnection(inlayer_p, outlayer_p);
+    con_p->get_parameters()[0] = 2.0; 
+    con_p->set_mode(Component::Sequential);
+    
+    Network* net_p= new Network();
+    net_p->add_module(inlayer_p, Network::InputModule);
+    net_p->add_module(outlayer_p, Network::OutputModule);
+    net_p->add_connection(con_p);
+    net_p->set_mode(Component::Sequential);
+    
+    double* seq_p = new double[2];
+    seq_p[0] = 1;
+    seq_p[1] = 2;
+    Sequence seq(2, 1, seq_p);
+    
+    double* target_p = new double[1];
+    target_p[0] = 3.0;
+    
+    SupervisedDataset<Sequence, double*> ds(1, 1);
+    ds.append(seq, target_p);
+    
+    SemiSequentialBackprop trainer(*net_p, ds);
+    trainer.set_learningrate(1.);
+    trainer.train_stochastic();
+
+    EXPECT_DOUBLE_EQ(-9, con_p->get_derivatives()[0])
+        << "Derivatives not correct.";
+
+    EXPECT_DOUBLE_EQ(-7, con_p->get_parameters()[0])
+        << "Parameters not learned correctly.";
+}
+
+
+TEST(TestBackprop, TestStochasticStepSequenceSequence)
+{
+    LinearLayer* inlayer_p = new LinearLayer(1);
+    LinearLayer* outlayer_p = new LinearLayer(1);
+    inlayer_p->set_mode(Component::Sequential);
+    outlayer_p->set_mode(Component::Sequential);
+
+    FullConnection* con_p = new FullConnection(inlayer_p, outlayer_p);
+    con_p->get_parameters()[0] = 1.5; 
+    con_p->set_mode(Component::Sequential);
+    
+    Network* net_p= new Network();
+    net_p->add_module(inlayer_p, Network::InputModule);
+    net_p->add_module(outlayer_p, Network::OutputModule);
+    net_p->add_connection(con_p);
+    net_p->set_mode(Component::Sequential);
+    
+    double* inseq_p = new double[2];
+    inseq_p[0] = 1;
+    inseq_p[1] = 2;
+    Sequence inseq(2, 1, inseq_p);
+    
+    double* outseq_p = new double[2];
+    outseq_p[0] = 2;
+    outseq_p[1] = 4;
+    Sequence outseq(2, 1, outseq_p);
+    
+    SupervisedDataset<Sequence, Sequence> ds(1, 1);
+    ds.append(inseq, outseq);
+    
+    SequentialBackprop trainer(*net_p, ds);
+    trainer.set_learningrate(1.);
+    trainer.train_stochastic();
+
+    EXPECT_DOUBLE_EQ(2.5, con_p->get_derivatives()[0])
+        << "Derivatives not correct.";
+
+    EXPECT_DOUBLE_EQ(4, con_p->get_parameters()[0])
+        << "Parameters not learned correctly.";
+}
+
         
 }  // namespace
